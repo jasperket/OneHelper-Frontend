f2fc66acc9d2e64a9845a671e09be03ade3efb43
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-09-09 08:13:40 +0800

    Add layout for authorized users

--- src/components/layout/AuthHeader.tsx
+++ src/components/layout/AuthHeader.tsx
@@ -1,31 +1,39 @@
+import type { ReactNode } from "react";
 import { Link } from "react-router";
 
-export default function AuthHeader() {
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export default function AuthHeader({ children }: LayoutProps) {
   return (
-    <header className="bg-gray-50 text-gray-700">
-      <nav className="mx-auto flex max-w-7xl items-center p-4">
-        <h2 className="text-themeOrange text-2xl font-extrabold">
-          One<span className="text-themeGreen">Helper</span>
-        </h2>
-        <ul className="absolute left-2/4 flex -translate-x-2/4 items-center gap-4">
-          <Link to="/dashboard" className="text-sm hover:underline">
-            Dashboard
-          </Link>
-          <Link to="/schedule" className="text-sm hover:underline">
-            Schedule
-          </Link>
-          <Link to="/todo" className="text-sm hover:underline">
-            To-do
-          </Link>
-          <Link to="/sleep" className="text-sm hover:underline">
-            Sleep
-          </Link>
-        </ul>
-        <div className="ml-auto flex items-center gap-4">
-          <div className="rounded-full border border-black p-4"></div>
-          <p>Hello Norwen</p>
-        </div>
-      </nav>
-    </header>
+    <>
+      <header className="bg-gray-50 text-gray-700">
+        <nav className="mx-auto flex max-w-7xl items-center p-4">
+          <h2 className="text-themeOrange text-2xl font-extrabold">
+            One<span className="text-themeGreen">Helper</span>
+          </h2>
+          <ul className="absolute left-2/4 flex -translate-x-2/4 items-center gap-4">
+            <Link to="/dashboard" className="text-sm hover:underline">
+              Dashboard
+            </Link>
+            <Link to="/schedule" className="text-sm hover:underline">
+              Schedule
+            </Link>
+            <Link to="/todo" className="text-sm hover:underline">
+              To-do
+            </Link>
+            <Link to="/sleep" className="text-sm hover:underline">
+              Sleep
+            </Link>
+          </ul>
+          <div className="ml-auto flex items-center gap-4">
+            <div className="rounded-full border border-black p-4"></div>
+            <p>Hello Norwen</p>
+          </div>
+        </nav>
+      </header>
+      <main className="mx-auto max-w-7xl p-4 pt-8">{children}</main>
+    </>
   );
 }
--- src/pages/ToDo.tsx
+++ src/pages/ToDo.tsx
@@ -16,47 +16,48 @@ export default function ToDoPage() {
   const types = [...new Set(tasks.map((task) => task.type))];
   return (
     <>
-      <AuthHeader />
-      <main className="mx-auto grid max-w-7xl grid-cols-2 gap-32 p-4 pt-8 text-gray-700">
-        <div>
-          <h1 className="text-5xl font-extrabold">
-            <span className="text-orange-400">To-Do</span>{" "}
-            <span className="text-green-400">List</span>
-          </h1>
-          <div className="mt-4 space-y-4 rounded-xl bg-gray-200 p-4 text-black">
-            <div className="flex justify-between">
-              <Select defaultValue="all">
-                <SelectTrigger className="w-48 bg-gray-50 text-gray-900">
-                  <SelectValue placeholder="All Tasks" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="all">All Tasks</SelectItem>
-                  <SelectItem value="personal">Personal</SelectItem>
-                  <SelectItem value="work">Work</SelectItem>
-                </SelectContent>
-              </Select>
-              <Button className="bg-themeOrange cursor-pointer hover:bg-orange-600">
-                <Plus />
-                New Task
-              </Button>
-            </div>
-            <div className="mt-8 space-y-4">
-              <div className="flex items-center justify-between gap-4 rounded-lg bg-gray-50 p-4 text-gray-700">
-                <Checkbox className="data-[state=checked]:bg-themeOrange data-[state=checked]: data-[state=checked]:border-themeOrange border-themeOrange" />
-                <div className="flex-1">
-                  <p className="text-lg font-bold">Title is here</p>
-                  <p className="text-sm text-gray-500">Description is here</p>
-                </div>
-                <div className="flex gap-4">
-                  <Pencil />
-                  <Trash2 />
+      <AuthHeader>
+        <div className="grid grid-cols-2 gap-32 text-gray-700">
+          <div>
+            <h1 className="text-5xl font-extrabold">
+              <span className="text-orange-400">To-Do</span>{" "}
+              <span className="text-green-400">List</span>
+            </h1>
+            <div className="mt-4 space-y-4 rounded-xl bg-gray-200 p-4 text-black">
+              <div className="flex justify-between">
+                <Select defaultValue="all">
+                  <SelectTrigger className="w-48 bg-gray-50 text-gray-900">
+                    <SelectValue placeholder="All Tasks" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="all">All Tasks</SelectItem>
+                    <SelectItem value="personal">Personal</SelectItem>
+                    <SelectItem value="work">Work</SelectItem>
+                  </SelectContent>
+                </Select>
+                <Button className="bg-themeOrange cursor-pointer hover:bg-orange-600">
+                  <Plus />
+                  New Task
+                </Button>
+              </div>
+              <div className="mt-8 space-y-4">
+                <div className="flex items-center justify-between gap-4 rounded-lg bg-gray-50 p-4 text-gray-700">
+                  <Checkbox className="data-[state=checked]:bg-themeOrange data-[state=checked]: data-[state=checked]:border-themeOrange border-themeOrange" />
+                  <div className="flex-1">
+                    <p className="text-lg font-bold">Title is here</p>
+                    <p className="text-sm text-gray-500">Description is here</p>
+                  </div>
+                  <div className="flex gap-4">
+                    <Pencil />
+                    <Trash2 />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <NewTask />
         </div>
-        <NewTask />
-      </main>
+      </AuthHeader>
     </>
   );
 }
